8b7725066568d9bc3e4d5f35752d2128f030b417
hisimagination <joel.design@icloud.com>
2014-07-13 01:19:29 -0700

    Changed port

--- server/app.js
+++ server/app.js
@@ -5,10 +5,12 @@ var express = require('express');
 var _ = require('underscore');
 var app = express();
 
+var port = process.ENV.PORT or || 9000
+
 var server = require('http').createServer(app);
 
 require('./routes')(app);
 
-server.listen(9000, '127.0.0.1', function () {
+server.listen(port, '127.0.0.1', function () {
   console.log('Express server listening on %d, in %s mode', 9000);
 });
