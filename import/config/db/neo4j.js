6cac1e1de36adff17edf871b40f414e239d50dd4
hisimagination <joel.design@icloud.com>
2014-07-29 22:52:29 -0700

    Fix Cached files problem

--- import/config/db/neo4j.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var neo4j;
-
-  neo4j = require('node-neo4j');
-
-  module.exports.db = new neo4j('http://localhost:7474');
-
-}).call(this);
--- run/config/environment/test.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-  module.exports = {
-    mongo: {
-      uri: 'mongodb://localhost/tester-test'
-    }
-  };
-
-}).call(this);
