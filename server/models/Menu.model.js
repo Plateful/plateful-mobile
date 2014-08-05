05af53f09d25efd1d7348b4b2e0ca78e2d8a3726
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-08-05 16:10:44 -0700

    Clean-up and comment server API models

--- server/models/Menu.model.js
+++ server/models/Menu.model.js
@@ -1,63 +1,55 @@
-(function() {
-  var Menu, db;
+var db = require('../config/neo4j').db;
 
-  db = require('../config/neo4j').db;
+var Menu = function() {
+  this.query = db.cypherQuery;
+};
 
-  Menu = function() {
-    return this.query = db.cypherQuery;
+Menu.prototype.all = function(callback) {
+  var q = "MATCH (menu:Menu) RETURN menu";
+  this.query(q, function(err, result) {
+    callback(err, result.data);
+  });
+};
+
+Menu.prototype.find = function(menu_id, callback) {
+  var params = {
+    menu_id: menu_id
   };
+  var q = "Match (m:Menu) WHERE m.locu_id = {menu_id} RETURN m";
+  this.query(q, params, function(err, result) {
+    callback(err, result.data);
+  });
+};
 
-  Menu.prototype.all = function(callback) {
-    var q;
-    q = "MATCH (menu:Menu) RETURN menu";
-    return this.query(q, function(err, result) {
-      return callback(err, result.data);
-    });
+Menu.prototype.create = function(menu, callback) {
+  var params = {
+    menu: menu
   };
+  var q = "CREATE (menu:Menu {menu}) RETURN menu";
+  this.query(q, params, function(err, result) {
+    callback(err, result.data);
+  });
+};
 
-  Menu.prototype.find = function(menu_id, callback) {
-    var params, q;
-    params = {
-      menu_id: menu_id
-    };
-    q = "Match (m:Menu) WHERE m.locu_id = {menu_id} RETURN m";
-    return this.query(q, params, function(err, result) {
-      return callback(err, result.data);
-    });
+Menu.prototype.update = function(menu_id, menu, callback) {
+  var params = {
+    menu_id: menu_id,
+    changes: menu
   };
+  var q = "";
+  this.query(q, params, function(err, result) {
+    callback(err, result.data);
+  });
+};
 
-  Menu.prototype.create = function(menu, callback) {
-    var params, q;
-    q = "CREATE (menu:Menu {menu}) RETURN menu";
-    params = {
-      menu: menu
-    };
-    return this.query(q, params, function(err, result) {
-      return callback(err, result.data);
-    });
+Menu.prototype.destroy = function(menu_id, callback) {
+  var params = {
+    menu_id: menu_id
   };
+  var q = "";
+  db.cypherQuery(q, params, function(err, result) {
+    callback(err, result.data);
+  });
+};
 
-  Menu.prototype.update = function(menu_id, menu, callback) {
-    var params, q;
-    params = {
-      menu_id: menu_id,
-      changes: menu
-    };
-    q = "";
-    return this.query(q, params, function(err, result) {
-      return callback(err, result.data);
-    });
-  };
-
-  Menu.prototype.destroy = function(menu_id, callback) {
-    var params, q;
-    params = {
-      menu_id: menu_id
-    };
-    q = "";
-    return db.cypherQuery(q, params, function(err, result) {
-      return callback(err, result.data);
-    });
-  };
-
-}).call(this);
+module.exports = new Menu();
