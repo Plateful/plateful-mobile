ff8857fe62ac3f2b1257741178f8d506693577be
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-08-05 16:01:09 -0700

    Clean-up and comment server routes and app

--- server/app.js
+++ server/app.js
@@ -1,30 +1,25 @@
-(function() {
-  var app, config, exports, express, server;
+// Set default node environment to development.
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-  process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var express = require('express');
+var config = require('./config/environment');
 
-  express = require('express');
+// Populate DB with sample data.
+if (config.seedDB) {
+  require('./config/seed');
+}
 
-  config = require('./config/environment');
+// Setup server.
+var app = express();
+var server = require('http').createServer(app);
+require('./config/express')(app);
+require('./routes').applyRoutes(app);
+require('./config/api/locu').testApi();
 
-  if (config.seedDB) {
-    require('./config/seed');
-  }
+// Start server.
+server.listen(config.port, config.ip, function() {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+});
 
-  app = express();
-
-  server = require('http').createServer(app);
-
-  require('./config/express')(app);
-
-  require('./routes').applyRoutes(app);
-
-  require('./config/api/locu').testApi();
-
-  server.listen(config.port, config.ip, function() {
-    return console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
-  });
-
-  exports = module.exports = app;
-
-}).call(this);
+// Expose app.
+exports = module.exports = app;
