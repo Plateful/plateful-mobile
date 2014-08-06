9e63a305da82201e09cd4d6b9efbcc486685b281
hisimagination <joel.design@icloud.com>
2014-08-06 14:47:38 -0700

    Update fix bug at App.js

--- app/js/App.js
+++ app/js/App.js
@@ -27,40 +27,40 @@
     });
   });
 
-  this.BaseCtrl = (function() {
-    BaseCtrl.inject = function() {
-      var args;
-      args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
-      return this.$inject = args;
-    };
-
-    function BaseCtrl() {
-      var args, fn, index, key, _i, _len, _ref, _ref1;
-      args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
-      _ref = this.constructor.$inject;
-      for (index = _i = 0, _len = _ref.length; _i < _len; index = ++_i) {
-        key = _ref[index];
-        this[key] = args[index];
-      }
-      _ref1 = this.constructor.prototype;
-      for (key in _ref1) {
-        fn = _ref1[key];
-        if (typeof fn !== 'function') {
-          continue;
-        }
-        if ((key === 'constructor' || key === 'initialize') || key[0] === '_') {
-          continue;
-        }
-        this.$scope[key] = (typeof fn.bind === "function" ? fn.bind(this) : void 0) || _.bind(fn, this);
-      }
-      if (typeof this.initialize === "function") {
-        this.initialize();
-      }
-    }
-
-    return BaseCtrl;
-
-  })();
+  // this.BaseCtrl = (function() {
+  //   BaseCtrl.inject = function() {
+  //     var args;
+  //     args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+  //     return this.$inject = args;
+  //   };
+  //
+  //   function BaseCtrl() {
+  //     var args, fn, index, key, _i, _len, _ref, _ref1;
+  //     args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+  //     _ref = this.constructor.$inject;
+  //     for (index = _i = 0, _len = _ref.length; _i < _len; index = ++_i) {
+  //       key = _ref[index];
+  //       this[key] = args[index];
+  //     }
+  //     _ref1 = this.constructor.prototype;
+  //     for (key in _ref1) {
+  //       fn = _ref1[key];
+  //       if (typeof fn !== 'function') {
+  //         continue;
+  //       }
+  //       if ((key === 'constructor' || key === 'initialize') || key[0] === '_') {
+  //         continue;
+  //       }
+  //       this.$scope[key] = (typeof fn.bind === "function" ? fn.bind(this) : void 0) || _.bind(fn, this);
+  //     }
+  //     if (typeof this.initialize === "function") {
+  //       this.initialize();
+  //     }
+  //   }
+  //
+  //   return BaseCtrl;
+  //
+  // })();
 
   for (k in GLOBALS) {
     v = GLOBALS[k];
