136e0ced30bc701db51ae6073c373242ff8fa69d
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-08-10 18:32:30 -0700

    Clean-up settings controller

--- app/js/modules/tabs/settings/controllers/SettingsCtrl.js
+++ app/js/modules/tabs/settings/controllers/SettingsCtrl.js
@@ -1,48 +1,9 @@
 (function() {
-  var SettingsCtrl = function($scope, $ionicModal, Auth, User) {
+  var SettingsCtrl = function($scope) {
     var vm = this;
-
-    $ionicModal
-      .fromTemplateUrl("js/modules/tabs/settings/views/loginModal.html", {
-        scope: $scope,
-        animation: "slide-in-up"
-      })
-      .then(function(modal) {
-          vm.loginModal = modal;
-      });
-
-    $ionicModal
-      .fromTemplateUrl("js/modules/tabs/settings/views/signupModal.html", {
-        scope: $scope,
-        animation: "slide-in-up"
-      })
-      .then(function(modal) {
-          vm.signupModal = modal;
-      });
-
-    vm.openModal  = openModal;
-    vm.closeModal = closeModal;
-    vm.signup     = signup;
-    vm.login      = login;
-
-    //////////////////////
-
-    function openModal() {
-      vm.loginModal.show();
-    };
-    function closeModal(){
-      vm.loginModal.hide();
-    };
-    function signup() {
-      User.signup(vm.username, vm.password);
-    };
-    function login() {
-      User.login(vm.username, vm.password);
-    };
-
   };
   SettingsCtrl
-    .$inject = ['$scope', '$ionicModal', 'Auth', 'User'];
+    .$inject = ['$scope'];
   angular
     .module('app.modules.tabs.settings.controllers', [])
     .controller('SettingsCtrl', SettingsCtrl);
