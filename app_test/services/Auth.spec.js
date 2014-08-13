b14a5ffcd6e4f74ae8c5b857a7a1358b94a7c9cd
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-08-13 11:20:42 -0700

    Store user data in session on facebook user creation

--- app_test/services/Auth.spec.js
+++ app_test/services/Auth.spec.js
@@ -5,29 +5,22 @@ describe('Auth', function(){
     beforeEach(angular.mock.module('app'));
     //mock the controller for the same reason and include $rootScope and $controller
     beforeEach(angular.mock.inject(function(Auth){
-
         auth = Auth;
         Auth.setAuthToken(3333, 'joelcox@hisimagination.com', '111222333444555', 'joelcoxokc');
     }));
     it('Should Store a session token and email of the user on login', function(){
-
         var id = localStorage.getItem('user_id');
         var email = localStorage.getItem('user_email');
         var token = localStorage.getItem('user_token');
         expect(id).toBe('3333');
         expect(email).toBe('joelcox@hisimagination.com');
         expect(token).toBe('111222333444555');
-
     });
     it('Should has an is Signed in method', function(){
-
       expect(auth.isSignedIn).toBeDefined();
-
     });
     it('Should have a refreshSession method', function(){
-
       expect(auth.resetSession).toBeDefined();
-
     });
     it('Should Is SignedIn Should return true if user is signed in', function(){
       expect(auth.isSignedIn()).toBe(true);
@@ -39,5 +32,4 @@ describe('Auth', function(){
       expect(email).toBe(null);
       expect(token).toBe(null);
     });
-
 });
