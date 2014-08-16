bb1a446e0fd80d360343ddae93a33d8d55ad4492
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-08-16 13:06:01 -0700

    Complete list model server integration testing

--- server/models/List.model.spec.js
+++ server/models/List.model.spec.js
@@ -1,5 +1,5 @@
 var request = require('supertest'),
-    express = require('express'),
+    should = require('should'),
     app = require('../app'),
     db = require('../config/neo4j.js').db,
     url = '/api/v1';
@@ -10,21 +10,21 @@ describe('List API', function() {
     var q = [
       "CREATE (u:USER {username: 'TestUser'}) ",
       "CREATE (u)-[:HAS_BOOKMARKS]->(d:USER_BOOKMARKS) ",
-      "CREATE (u)-[:HAS_COLLECTIONS]->(d:USER_COLLECTIONS) ",
-      "CREATE d-[:BOOKMARKED]->(i:ITEM{name: 'Duck Confit'}) ",
-      "CREATE d-[:BOOKMARKED]->(i:ITEM{name: 'Fantastic Bowl'}) ",
-      "CREATE d-[:BOOKMARKED]->(i:ITEM{name: '21 Hayes'}) ",
-      "CREATE d-[:COLLECTED]->(i:ITEM{name: 'Chesapeake Bay Soft Shell Crab Salad'}) ",
-      "CREATE d-[:COLLECTED]->(i:ITEM{name: 'Goat Cheese Cake and Beet Ice Cream'})"
+      "CREATE (u)-[:HAS_COLLECTIONS]->(c:USER_COLLECTIONS) ",
+      "CREATE (d)-[:BOOKMARKED]->(:ITEM {name: 'Duck Confit'}) ",
+      "CREATE (d)-[:BOOKMARKED]->(:ITEM {name: 'Fantastic Bowl'}) ",
+      "CREATE (d)-[:BOOKMARKED]->(:ITEM {name: '21 Hayes'}) ",
+      "CREATE (c)-[:COLLECTED]->(:ITEM {name: 'Chesapeake Bay Soft Shell Crab Salad'}) ",
+      "CREATE (c)-[:COLLECTED]->(:ITEM {name: 'Goat Cheese Cake and Beet Ice Cream'})"
     ].join('');
     db.cypherQuery(q, function(err, result) {
       done();
     });
   });
-  after(function() {
+  after(function(done) {
     var q = [
-      "Match (u:USER{username:'TestUser'})-[hb:HAS_BOOKMARKS]->(ub:USER_BOOKMARKS)-[b:BOOKMARKED]->(),",
-      "Match (u)-[hc:HAS_COLLECTIONS]->(uc:USER_COLLECTIONS)-[c:COLLECTED]->() ",
+      "MATCH (u:USER{username:'TestUser'})-[hb:HAS_BOOKMARKS]->(ub:USER_BOOKMARKS)-[b:BOOKMARKED]->(),",
+      "(u)-[hc:HAS_COLLECTIONS]->(uc:USER_COLLECTIONS)-[c:COLLECTED]->() ",
       "DELETE u, hb, ub, b, hc, uc, c"
     ].join('');
     db.cypherQuery(q, function(err, result) {
@@ -32,17 +32,21 @@ describe('List API', function() {
     });
   })
   describe('GET /lists', function() {
+    var data = 10;
     it('should return an array of bookmarks and collections', function(done) {
       request(app)
-        .get(url + '/lists/John')
+        .get(url + '/lists/TestUser')
         .expect(200)
         .end(function(err, res) {
-          var data = JSON.parse(res.text);
-          assert.equal(Array.isArray(data.itemArray), true);
-          assert.equal(data.itemArray.length, 5);
-          assert.equal(data.itemArray[0], 'Duck Confit');
+          data = JSON.parse(res.text);
+          (Array.isArray(data.itemArray)).should.be.exactly(true);
           done();
         });
+    });
+    it('should return the correct items', function() {
+      (data.itemArray.length).should.be.exactly(5);
+      (data.itemArray[0].name).should.be.exactly('Duck Confit');
+      (data.itemArray[4].name).should.be.exactly('Goat Cheese Cake and Beet Ice Cream');
     });
   });
 });
