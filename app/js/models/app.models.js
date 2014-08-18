NeoTim/clurtch

Architecture Update: Convert all modules | app.factories - to - app.models

app/js/factories/app.models.js
@@ -1,0 +1,20 @@
+
+/*
+ *
+ *   app.factories all are all the factories that primarily deal with
+ *   Restful calls to the server
+ *
+ */
+
+(function() {
+  angular
+    .module('app.models', [
+      'app.models.menu',
+      'app.models.item',
+      'app.models.review',
+      'app.models.user',
+      'app.models.photo',
+      'app.models.fbLogin'
+    ]);
+
+}).call(this);

Plateful/plateful-mobile

app/js/models/app.models.js
@@ -1,0 +1,20 @@
+
+/*
+ *
+ *   app.factories all are all the factories that primarily deal with
+ *   Restful calls to the server
+ *
+ */
+
+(function() {
+  angular
+    .module('app.models', [
+      'app.model.menu',
+      'app.model.item',
+      'app.model.review',
+      'app.model.user',
+      'app.model.photo',
+      'app.model.fbLogin'
+    ]);
+
+}).call(this);