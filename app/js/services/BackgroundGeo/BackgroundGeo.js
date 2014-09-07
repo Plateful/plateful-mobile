7cc97e7e7c3003282eb0c9f9c8376c5327aa1090
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-09-07 02:27:59 -0700

    Fix bug in from location

--- app/js/services/BackgroundGeo/BackgroundGeo.js
+++ app/js/services/BackgroundGeo/BackgroundGeo.js
@@ -7,7 +7,7 @@
         from = new google.maps.LatLng(data.latitude, data.longitude);
       });
 
-    instance = {
+    var instance = {
       current: current,
       distance: distance
     };
@@ -18,14 +18,15 @@
       var locator = $q.defer();
       locator.resolve(window.currLocation.coords);
       return locator.promise;
-    }
+    };
+
     function distance(lat, lng){
       var dist, to;
       to = new google.maps.LatLng(lat, lng);
+      from = new google.maps.LatLng(window.currLocation.coords.latitude, window.currLocation.coords.longitude);
       dist = google.maps.geometry.spherical.computeDistanceBetween(from, to) * 0.000621371192;
       return Math.floor((dist + 0.005)*100) / 100;
-
-    }
+    };
 
     return instance;
   };
